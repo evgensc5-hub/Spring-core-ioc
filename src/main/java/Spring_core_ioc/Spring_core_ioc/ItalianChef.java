be6b518bce1256e936eb0ab5e57b5b68e04f8d8a
Eugen Scutaru <evgensc5@gmail.com>
2026-08-25 20:30:56 +0300

    Create ne class, implement Chef and methods

--- src/main/java/Spring_core_ioc/Spring_core_ioc/ItalianChef.java
+++ src/main/java/Spring_core_ioc/Spring_core_ioc/ItalianChef.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class ItalianChef implements Chef {
 
     @Override
-    public String getDailyRecipe() {
+    public String getDailyRecipe()  {
         return "Prepare pasta al pomodoro with fresh tomatoes and basil!";
     }
 }
