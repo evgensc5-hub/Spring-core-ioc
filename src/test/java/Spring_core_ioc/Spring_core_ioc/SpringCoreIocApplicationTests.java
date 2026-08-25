b8a11ba82694275191e4a89504fd59c3e4cd192d
Eugen Scutaru <evgensc5@gmail.com>
2026-08-25 20:15:37 +0300

    Initial commit

---
+++ src/test/java/Spring_core_ioc/Spring_core_ioc/SpringCoreIocApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_core_ioc.Spring_core_ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreIocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
